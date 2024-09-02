2f1d0aef6db1f80816386a6620d7cab08e38e0b5
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-03 03:13:02 +0400

    lesson2--lesson practice2
    for and if operator

---
+++ homeworks/src/homeworks/simpleoperators.java
@@ -0,0 +1,28 @@
+package homeworks;
+
+public class simpleoperators {
+	public static void main(String[] args) {
+		for (int n = 1; n <= 5; n++) { // 1
+			System.out.println(n);
+		}
+
+		int sum = 0; // 7
+		for (int n = 1; n <= 100; n++) {
+			sum = sum + n;
+		}
+		System.out.println(sum);
+
+		for (int n = 1; n <= 100; n++) { // 8
+			if (n % 7 == 0) {
+				System.out.println(n);
+			}
+		}
+		for (int n = 1; n <= 200; n++) { // 10
+			if (n % 3 == 0) {
+				if (n % 7 == 0) {
+					System.out.println(n);
+				}
+			}
+		}
+	}
+}
