e3be53089d2669e8e9ba60b1e41b4fb8cdbdc35c
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-03 03:26:59 +0400

    lesson2--lesson practice3
    for operator

---
+++ homeworks/src/homeworks/LoopOperators.java
@@ -0,0 +1,12 @@
+package homeworks;
+
+public class LoopOperators {
+public static void main(String[] args) {
+	for (int n=50; n>=20; n--) {  //1
+		System.out.println(n);
+	}
+    for (double i = 0; i <= 5; i += 0.1) {  // 'int' yerine 'double' kullanıldı
+        System.out.println(i);
+	}
+}
+}
