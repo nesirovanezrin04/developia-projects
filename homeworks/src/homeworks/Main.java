918e22e2bc47bebfbe5495ed1811d35d44dd6c8b
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-03 02:05:27 +0400

    lesson1--homework1
    variables

---
+++ homeworks/src/homeworks/Main.java
@@ -0,0 +1,26 @@
+package homeworks;
+
+public class Main {
+public static void main(String[] args) {
+	int age=7;  //1
+	System.out.println(age);
+	age=14;     //2
+	System.out.println(age);
+	byte b=7;   //3
+	System.out.println(b);
+	short s=7;  //4
+	System.out.println(s);
+	long l=7;   //5
+	System.out.println(l);
+	double d=7.9D;  //6
+	System.out.println(d);
+	float f=7.9F;   //7
+	System.out.println(f);
+	char c='A';     //8
+	System.out.println(c);
+	boolean hasHome=true;    //9
+	System.out.println(hasHome);
+	
+	
+}
+}
