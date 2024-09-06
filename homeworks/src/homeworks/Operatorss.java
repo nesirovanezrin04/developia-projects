a5fb10d844ec3bdfb94550116f289c913104d082
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-06 04:47:01 +0400

    lesson2--homework2
    for operator

---
+++ homeworks/src/homeworks/Operatorss.java
@@ -0,0 +1,15 @@
+package homeworks;
+
+public class Operatorss {
+	public static void main(String[] args) {
+		for (int i = 15; i <= 40; i++) { // 1
+			System.out.println(i);
+		}
+		for (int i = 20; i <= 50; i++) { // 2
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+}
