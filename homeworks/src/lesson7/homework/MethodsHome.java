b12d5910d4a73c100f9868d04b99c865ac2b2927
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-20 01:20:10 +0400

    lesson4--lesson homework1
    to create method

---
+++ homeworks/src/lesson7/homework/MethodsHome.java
@@ -0,0 +1,9 @@
+package lesson7.homework;
+
+public class MethodsHome {
+	public void printNumbers(int begin, int end) {
+		for (int i = begin; i <= end; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ homeworks/src/lesson7/homework/MethodsHomeMain.java
@@ -0,0 +1,10 @@
+package lesson7.homework;
+
+public class MethodsHomeMain {
+	public static void main(String[] args) {
+		MethodsHome object1 = new MethodsHome();
+
+		object1.printNumbers(1, 10);
+		object1.printNumbers(15, 20);
+	}
+}
