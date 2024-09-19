bde7d90921c6391cdea19c4e718e25012e19f98b
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-09-20 00:32:58 +0400

    lesson4--lesson practice1
    to create method

---
+++ homeworks/src/lesson7/homework/MethodsMain.java
@@ -0,0 +1,13 @@
+package lesson7.homework;
+
+public class MethodsMain {
+	public static void main(String[] args) {
+		Methods object1 = new Methods();
+
+		object1.printName(2, "Nezrin");
+		object1.printName(3, "Java");
+		object1.printName(4, "Math");
+
+	}
+
+}
