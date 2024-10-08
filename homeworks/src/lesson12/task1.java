623b0de0d5c83e1edd1e28c80f6611ca64e929d1
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:28:49 +0400

    Write a Java program to get the character at the given index within the string.

---
+++ homeworks/src/lesson12/task1.java
@@ -0,0 +1,15 @@
+package lesson12;
+
+public class task1 {
+	public static void main(String[] args) {
+		String str = "Java Exercises!";
+
+		System.out.println("Original String = " + str);
+
+		char index1 = str.charAt(0);
+		char index2 = str.charAt(10);
+
+		System.out.println("The character at position 0 is " + index1);
+		System.out.println("The character at position 10 is " + index2);
+	}
+}
