894e46dca28e846fe31ab912c450f4886092f16f
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:32:49 +0400

    Write a Java program to convert all characters in a string to uppercase.

---
+++ homeworks/src/lesson12/task8.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+public class task8 {
+	public static void main(String[] args) {
+
+		        String originalString = "Welcome to the World of Programming.";
+
+		        String uppercaseString = originalString.toUpperCase();
+
+		        System.out.println("Original String: " + originalString);
+		        System.out.println("Uppercase String: " + uppercaseString);
+		    }
+		
+
+	}
+
