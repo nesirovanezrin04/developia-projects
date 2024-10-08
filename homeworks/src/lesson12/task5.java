cb254703039a5690c313c229fd99baf61355e55a
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:30:38 +0400

    Write a Java program to check whether two String objects contain the same data.

---
+++ homeworks/src/lesson12/task5.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+public class task5 {
+	public static void main(String[] args) {
+		String str1 = "Hello, World!";
+		String str2 = "Hello, Java!";
+
+		if (str1.equals(str2)) {
+			System.out.println("str1 and str2 contain the same data.");
+		} else {
+			System.out.println("str1 and str2 do not contain the same data.");
+		}
+
+	}
+
+}
