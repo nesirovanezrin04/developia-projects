a2635e57062bf97c558b6f9290638fee156538af
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:30:15 +0400

    Write a Java program to check whether a given string ends with another string.

---
+++ homeworks/src/lesson12/task4.java
@@ -0,0 +1,17 @@
+package lesson12;
+
+public class task4 {
+	public static void main(String[] args) {
+
+		String mainString = "Java Programming Language";
+
+		String suffix = "Language";
+
+		if (mainString.endsWith(suffix)) {
+			System.out.println("The main string ends with the suffix \"" + suffix + "\".");
+		} else {
+			System.out.println("The main string does not end with the suffix \"" + suffix + "\".");
+		}
+
+	}
+}
