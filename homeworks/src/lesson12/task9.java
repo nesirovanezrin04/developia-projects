27f98d9fe2a6e476fcae710d78e0cabcaee9e1c2
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:32:15 +0400

    29. Write a Java program to convert all characters in a string to lowercase.

---
+++ homeworks/src/lesson12/task9.java
@@ -0,0 +1,14 @@
+package lesson12;
+
+public class task9 {
+	public static void main(String[] args) {
+
+		String originalString = "Welcome to the World of Programming.";
+
+		String lowercaseString = originalString.toLowerCase();
+
+		System.out.println("Original String: " + originalString);
+		System.out.println("Lowercase String: " + lowercaseString);
+	}
+
+}
