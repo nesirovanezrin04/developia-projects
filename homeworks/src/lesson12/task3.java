af2e46b74ea52609b4f5fa43f3f1d38d0fb410b6
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:29:35 +0400

    Write a Java program to concatenate a given string to the end of another string.

---
+++ homeworks/src/lesson12/task3.java
@@ -0,0 +1,14 @@
+package lesson12;
+
+public class task3 {
+	public static void main(String[] args) {
+		String str1 = "Hi,";
+		String str2 = "Alice";
+		        
+		String result = str1.concat(str2);
+		        
+
+		System.out.println("After concatenation: " + result);
+		    }
+
+}
