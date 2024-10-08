017012fc210b2ea0db6e3bd3cff1c145d9a6454a
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-08 16:30:57 +0400

    Write a Java program to compare a given string to another string, ignoring case considerations.

---
+++ homeworks/src/lesson12/task6.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+public class task6 {
+	public static void main(String[] args) {
+
+		String str1 = "Java Programming";
+		String str2 = "Java is fun";
+
+		if (str1.equalsIgnoreCase(str2)) {
+			System.out.println("str1 and str2 are equal (ignoring case).");
+		} else {
+			System.out.println("str1 and str2 are not equal (ignoring case).");
+		}
+	}
+
+}
