97748765431cf196a12acf4b019cbb0b53173336
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-10-21 22:13:09 +0400

    lesson13--OOP
    slide8--homework8
    create class,create method,extend(varislik),create constructor

---
+++ homeworks/src/lesson13/Employee.java
@@ -0,0 +1,27 @@
+package lesson13;
+
+public class Employee extends Person {
+    double salary;
+    String department;
+    String username;
+    String password;
+
+    public Employee(int id, String name, String surname, int age, String phone, String address,
+                    double salary, String department, String username, String password) {
+        super(id, name, surname, age, phone, address);
+        this.salary = salary;
+        this.department = department;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Salary: " + salary);
+        System.out.println("Department: " + department);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+}
+
---
+++ homeworks/src/lesson13/Mainn.java
@@ -0,0 +1,10 @@
+package lesson13;
+
+public class Mainn {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1, "John", "Doe", 30, "123456789", "123 Street",
+                                    5000.0, "IT", "johndoe", "password123");
+
+        employee1.printInfo();
+    }
+}
