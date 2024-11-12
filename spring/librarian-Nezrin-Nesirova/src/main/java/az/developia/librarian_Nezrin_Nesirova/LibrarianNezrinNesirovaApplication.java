d91eb16fbdb74930959f5f640547ea88dcf64530
nesirovanezrin04 <nesirova.nezrin04@gmail.com>
2024-11-12 14:01:33 +0400

    lesson21
    librarian app created

---
+++ spring/librarian-Nezrin-Nesirova/src/main/java/az/developia/librarian_Nezrin_Nesirova/LibrarianNezrinNesirovaApplication.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_Nezrin_Nesirova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarianNezrinNesirovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarianNezrinNesirovaApplication.class, args);
+	}
+
+}
---
+++ spring/librarian-Nezrin-Nesirova/src/test/java/az/developia/librarian_Nezrin_Nesirova/LibrarianNezrinNesirovaApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.librarian_Nezrin_Nesirova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianNezrinNesirovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
